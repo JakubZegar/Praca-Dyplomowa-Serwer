d5e46511ca7cbeb04872947e2d0de802235ff2cb
Jakub Zegar <jakubzegar3@gmail.com>
2020-10-27 14:32:39 +0100

    project init

---
+++ src/main/java/com/jakubzegar/pracadyplomowaserwer/PracaDyplomowaSerwerApplication.java
@@ -0,0 +1,13 @@
+package com.jakubzegar.pracadyplomowaserwer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracaDyplomowaSerwerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracaDyplomowaSerwerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jakubzegar/pracadyplomowaserwer/PracaDyplomowaSerwerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakubzegar.pracadyplomowaserwer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracaDyplomowaSerwerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
